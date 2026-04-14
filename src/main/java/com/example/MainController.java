dd2ef238d1c6bd1c63debfdb99919432c79c8a8a
Gyenge Elemér <gyenge@valahol>
2026-04-14 10:02:10 +0200

    Jelölőnégyzetek megjelennek

--- src/main/java/com/example/MainController.java
+++ src/main/java/com/example/MainController.java
@@ -2,10 +2,13 @@ package com.example;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class MainController {
+    int n = 90;
+    CheckBox[] boxes = new CheckBox[n];
 
     @FXML
     private TextField countNums;
@@ -13,9 +16,19 @@ public class MainController {
     @FXML
     private GridPane gridPane;
 
-    @FXML
-    void onClickSaveButton(ActionEvent event) {
-
+    @FXML void initialize() {
+        for(int i=0; i<n; i++) {
+            boxes[i] = new CheckBox();
+            gridPane.add(boxes[i], i%7, i/7);
+        }
+        
+        
     }
 
+    @FXML
+    void onClickSaveButton(ActionEvent event) {
+        this.startSave();
+    }
+
+    void startSave() {}
 }
